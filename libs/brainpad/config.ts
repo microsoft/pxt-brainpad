93fbd3beabd09db2fe7430b8bbc8cedc6822122f
Michal Moskal <michal@moskal.me>
2017-12-12 11:01:34 +0000

    Add pinout

--- libs/brainpad/config.ts
+++ libs/brainpad/config.ts
@@ -1,46 +1,44 @@
+// http://files.ghielectronics.com/downloads/Schematics/FEZ/BrainPad%20BP2%20Schematic.pdf
+
 namespace config {
-    export const NUM_NEOPIXELS = 10;
-    export const DEFAULT_BUTTON_MODE = DAL.BUTTON_ACTIVE_HIGH_PULL_DOWN;
+    export const DEFAULT_BUTTON_MODE = DAL.BUTTON_ACTIVE_LOW_PULL_UP;
 
-    export const PIN_FLASH_MISO = DAL.PA16;
-    export const PIN_FLASH_MOSI = DAL.PA20;
-    export const PIN_FLASH_SCK = DAL.PA21;
-    export const PIN_FLASH_CS = DAL.PB22;
-    export const PIN_MIC_DATA = DAL.PA08;
-    export const PIN_MIC_CLOCK = DAL.PA10;
-    export const PIN_BTN_SLIDE = DAL.PA15;
-    export const PIN_NEOPIXEL = DAL.PB23;
-    export const PIN_SPEAKER_AMP = DAL.PA30;
-    export const PIN_MICROPHONE = DAL.PA08;
-    export const PIN_LIGHT = DAL.PA11;
-    export const PIN_ACCELEROMETER_SDA = DAL.PA00;
-    export const PIN_ACCELEROMETER_SCL = DAL.PA01;
-    export const PIN_ACCELEROMETER_INT = DAL.PA13;
-    export const PIN_TEMPERATURE = DAL.PA09;
-    export const PIN_IR_OUT = DAL.PA23;
-    export const PIN_IR_IN = DAL.PA12;
-    export const PIN_BTN_A = DAL.PA28;
-    export const PIN_BTN_B = DAL.PA14;
+    export const PIN_LIGHT = DAL.PB_1;
+    export const PIN_TEMPERATURE = DAL.PB_0;
 
-    export const PIN_A0 = DAL.PA02;
-    export const PIN_A1 = DAL.PA05;
-    export const PIN_A2 = DAL.PA06;
-    export const PIN_A3 = DAL.PA07;
-    export const PIN_A4 = DAL.PB03;
-    export const PIN_A5 = DAL.PB02;
-    export const PIN_A6 = DAL.PB09;
-    export const PIN_A7 = DAL.PB08;
-    export const PIN_SCL = DAL.PB03;
-    export const PIN_SDA = DAL.PB02;
-    export const PIN_RX = DAL.PB09;
-    export const PIN_TX = DAL.PB08;
-    export const PIN_A8 = DAL.PA11;
-    export const PIN_A9 = DAL.PA09;
-    export const PIN_D4 = DAL.PA28;
-    export const PIN_D5 = DAL.PA14;
-    export const PIN_D7 = DAL.PA15;
-    export const PIN_D8 = DAL.PB23;
-    export const PIN_D13 = DAL.PA17;
-    export const PIN_LED = DAL.PA17;
-    export const PIN_A10 = DAL.PA08;
+    export const PIN_BTN_A = DAL.PA_15;
+    export const PIN_BTN_B = DAL.PA_5;
+    export const PIN_BTN_C = DAL.PC_13;
+    export const PIN_BTN_D = DAL.PB_10;
+
+    export const PIN_SCL = DAL.PB_6;
+    export const PIN_SDA = DAL.PB_7;
+    export const PIN_RX = DAL.PA_10;
+    export const PIN_TX = DAL.PA_9;
+
+    export const PIN_AN = DAL.PA_7;
+    export const PIN_RST = DAL.PA_6;
+    export const PIN_CS = DAL.PC_3;
+    export const PIN_SCK = DAL.PB_3;
+    export const PIN_MISO = DAL.PB_4;
+    export const PIN_MOSI = DAL.PB_5;
+    export const PIN_PWM = DAL.PA_8;
+    export const PIN_INT = DAL.PA_2;
+    
+    export const PIN_ACCELEROMETER_SDA = PIN_SDA;
+    export const PIN_ACCELEROMETER_SCL = PIN_SCL;
+    //export const PIN_ACCELEROMETER_INT = NC;
+
+    // LCD also on the same I2C
+
+    export const PIN_LED_R = DAL.PC_9;
+    export const PIN_LED_G = DAL.PC_8;
+    export const PIN_LED_B = DAL.PC_6;
+
+    export const PIN_SERVO_1 = DAL.PA_3;
+    export const PIN_SERVO_2 = DAL.PA_0;
+
+    export const PIN_BUZZER = DAL.PB_8;
+
+    //export const PIN_LED = DAL.PA17;
 }
