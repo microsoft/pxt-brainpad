34bef95f1096543f75fed20197ea36e05579f6be
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-03-08 13:50:12 -0800

    bring back shims

---
+++ libs/lightbulb/shims.d.ts
@@ -0,0 +1,14 @@
+// Auto-generated. Do not edit.
+declare namespace lightbulb {
+
+    /**
+     * Set the rgb led to a specific red, green, blue color.
+     * @param red the red color
+     * @param green the green color
+     * @param blue the blue color
+     */
+    //% parts="rgbled" shim=lightbulb::setRGBLed
+    function setRGBLed(r: int32, g: int32, b: int32): void;
+}
+
+// Auto-generated. Do not edit. Really.
---
+++ libs/music/enums.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum SoundOutputDestination {
+    //% block="pin PWM"
+    PinPWM = 1,
+    //% block="speaker"
+    Speaker = 0,
+    }
+
+// Auto-generated. Do not edit. Really.
