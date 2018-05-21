bfe2cb26fdd8595de36b964bd21d9867e8762565
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-21 15:43:02 -0700

    fixing #86

--- libs/brainpad/images.ts
+++ libs/brainpad/images.ts
@@ -1,62 +1,62 @@
 namespace images {
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const butterfly1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const butterfly2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const cat1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const cat2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const chicken = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const cow = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const dog1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const dog2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const eyes1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const eyes2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const eyes3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const eyes4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const floppy1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const floppy2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const frown = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const globe = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const go = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const hSign = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const iSign = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const pig = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const smile = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const soccerBall = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const stegosaurus = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const stop = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const trex = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const triceratop = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const walker1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const walker2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const walker3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=display.showImage
+    //% fixedInstance jres
     export const walker4 = image.ofBuffer(hex``);
 }
