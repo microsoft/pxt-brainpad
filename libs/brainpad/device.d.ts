e6720cf77b09334f0c9da0a09b150765cb1859ac
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-25 08:57:08 -0700

    fixed casing

--- libs/brainpad/device.d.ts
+++ libs/brainpad/device.d.ts
@@ -30,28 +30,28 @@ declare namespace input {
     /**
      * Left button.
      */
-    //% block="button Left" weight=95 fixedInstance
+    //% block="button left" weight=95 fixedInstance
     //% shim=pxt::getButtonByPin(PA_15,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonL: Button;
 
     /**
      * Up button.
      */
-    //% block="button Up" weight=94 fixedInstance
+    //% block="button up" weight=94 fixedInstance
     //% shim=pxt::getButtonByPin(PA_5,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonU: Button;
 
     /**
      * Down button.
      */
-    //% block="button Down" weight=94 fixedInstance
+    //% block="button down" weight=94 fixedInstance
     //% shim=pxt::getButtonByPin(PB_10,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonD: Button;
 
     /**
      * Right button.
      */
-    //% block="button Right" weight=94 fixedInstance
+    //% block="button right" weight=94 fixedInstance
     //% shim=pxt::getButtonByPin(PC_13,BUTTON_ACTIVE_LOW_PULL_UP)
     const buttonR: Button;
 }
