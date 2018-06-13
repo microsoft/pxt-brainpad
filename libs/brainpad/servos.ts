fe8758fc06eb311e6206359412384b4589a8d752
greg-norris <greg.norris@ghielectronics.com>
2018-06-13 15:22:52 -0400

    fixing renaming of servos (#101)
    * changed namespace
    
    * updated pxt version
    
    * rename file
    
    * fix namespace

---
+++ libs/brainpad/servos.ts
@@ -0,0 +1,6 @@
+namespace servos {
+    //% fixedInstance block="servo 1"
+    export const servo1 = new servos.PinServo(pins.SERVO1);
+    //% fixedInstance block="servo 2"
+    export const servo2 = new servos.PinServo(pins.SERVO2);
+}
