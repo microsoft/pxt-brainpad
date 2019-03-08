f234ad3e6309ec9085c3cebb25541fa65c2f8fb3
Greg Norris <greg.norris@ghielectronics.com>
2019-03-08 15:58:06 -0500

    Commented out input.buttons in display override.

--- libs/display/targetoverrides.ts
+++ libs/display/targetoverrides.ts
@@ -1,2 +1,2 @@
-input.buttonUp.onEvent(ButtonEvent.Up, () => display.text.scroll(-3))
-input.buttonDown.onEvent(ButtonEvent.Down, () => display.text.scroll(3))               
+//input.buttonUp.onEvent(ButtonEvent.Up, () => display.text.scroll(-3))
+//input.buttonDown.onEvent(ButtonEvent.Down, () => display.text.scroll(3))               
